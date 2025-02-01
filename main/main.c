1e97bd2146d83b06492b44db7b8a99e84eadbb61
f4bjh <f4bjh_95@yahoo.fr>
2025-02-01 09:04:29 +0100

    7 - print current partition in console before all initialization

--- main/main.c
+++ main/main.c
@@ -5,10 +5,13 @@
 #include "freertos/queue.h"
 #include <esp_ota_ops.h>
 #include <nvs_flash.h>
+#include "esp_log.h"
 
 #include "http_server.h"
 #include "softap.h"
 #include "data.h"
+
+static const char* TAG = "main";
 
 void app_main(void) {
 	esp_err_t ret = nvs_flash_init();
@@ -17,15 +20,14 @@ void app_main(void) {
 		ESP_ERROR_CHECK(nvs_flash_erase());
 		ret = nvs_flash_init();
 	}
-
 	ESP_ERROR_CHECK(ret);
+
+	const esp_partition_t *partition = esp_ota_get_running_partition();
+	ESP_LOGI(TAG, "Currently running partition: %s", partition->label);
+
 	ESP_ERROR_CHECK(data_init());
 	ESP_ERROR_CHECK(softap_init());
 	ESP_ERROR_CHECK(http_server_init());
-
-	/* Mark current app as valid */
-	const esp_partition_t *partition = esp_ota_get_running_partition();
-	printf("Currently running partition: %s\r\n", partition->label);
 
 	esp_ota_img_states_t ota_state;
 	if (esp_ota_get_state_partition(partition, &ota_state) == ESP_OK) {
