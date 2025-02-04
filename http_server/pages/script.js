8580a8469c45d07afe845ed84afe294ca04b07de
Fabrice DECROP LONGET <f4bjh_95@yahoo.fr>
2025-02-04 09:48:42 +0100

    17 - align script.js with current java script  of index.html in main

--- http_server/pages/script.js
+++ http_server/pages/script.js
@@ -1,61 +1,12 @@
 let chip_model = '';
 let chip_revision = '';
+let counter =  `unknown`;
 
 socket = new WebSocket("ws://192.168.4.1/ws");
 
 socket.onopen = function () {
 	socket.send("Hello Server!");
 };
-
-// Gérer les messages reçus du serveur
-socket.addEventListener('message', (event) => {
-	json_data = JSON.parse(event.data);
-
-	// Afficher le tag hexadécimal dans la console
-	console.log(`Tag hexadécimal : ${json_data.tag}`);
-	console.log(`length : ${json_data.length}`);
-
-	// Récupérer l'élément HTML avec l'identifiant "message-container" de la page actuelle
-	messageContainer = document.getElementById('message-container');
-		
-	// Récupérer l'URL de la page actuelle
-	url = window.location.href;
-
-	// Extraire le paramètre d'identification de la page
-	pageId = url.split('/').pop(); // Supposons que l'ID de la page est le dernier segment de l'URL
-
-	// Filtrer les données en fonction de la page active
-	switch (pageId) {
-		case '':
-		if (json_data.tag === 0x01) {
-				chip_model = `chip_model : unknown<br>`;
-				if (json_data.length !== 0) {
-					chip_model = `${json_data.value}`;
-				}
-      		}
-		if (json_data.tag === 0x02) {
-				chip_revision = `chip_revision : unknown<br>`;
-				if (json_data.length !== 0) {
-					chip_revision = `${json_data.value}`;
-				}
-		}
-		messageContainer.innerHTML = `chip model : ${chip_model}<br>`;
-                messageContainer.innerHTML += `chip revision : ${chip_revision}<br>`;
-		break;
-
-		case 'frequencymeter':
-		if (json_data.tag === 0x03 || json_data.tag === 0x07) {
-			messageContainer.innerHTML += `Valeur champ ${json_data.tag} : ${json_data.value}<br>`;
-      		}
-		break;
-
-		case 'powermeter':
-	        if (json_data.tag === 0x46 || json_data.tag === 0xAB) {
-			messageContainer.innerHTML += `Valeur champ ${json_data.tag} : ${json_data.value}<br>`;
-		}
-		break;
-       	}
-});
 
 // Gérer les erreurs de connexion
 socket.addEventListener('error', (event) => {
@@ -65,4 +16,62 @@ socket.addEventListener('error', (event) => {
 // Gérer la fermeture de la connexion
 socket.addEventListener('close', (event) => {
 	console.log('Connexion WebSocket fermée');
+});
+
+// Gérer les messages reçus du serveur
+socket.addEventListener('message', (event) => {
+	const json_data = JSON.parse(event.data);
+
+	// Afficher le tag hexadécimal dans la console du navigateur
+	console.log(`t : ${json_data.t}`);
+	console.log(`l : ${json_data.l}`);
+	console.log(`v : ${json_data.v}`);
+
+	// Récupérer l'élément HTML avec l'identifiant "message-container" de la page actuelle
+	const messageContainer = document.getElementById('message-container');
+		
+	// Récupérer l'URL de la page actuelle
+	const url = window.location.href;
+
+	// Extraire le paramètre d'identification de la page
+	const pageId = url.split('/').pop(); // Supposons que l'ID de la page est le dernier segment de l'URL
+
+	// Filtrer les données en fonction de la page active
+	switch (pageId) {
+	case '':
+		if (json_data.t === 0x00) {
+			chip_model = `unable to read chip info`;
+			chip_revision = `unable to read chip info`;
+		}	
+		if (json_data.t === 0x01) {
+			if (json_data.l !== 0) {
+				chip_model = `${json_data.v}`;
+			}
+		}
+		if (json_data.t === 0x02) {
+			if (json_data.l !== 0) {
+				chip_revision = `${json_data.v}`;
+			}
+		}
+		if (json_data.t === 0x03) {
+			if (json_data.l !== 0) {
+				counter = `${json_data.v}`;
+			}
+		}
+
+		messageContainer.innerHTML = `chip model : ${chip_model}<br>`;
+                messageContainer.innerHTML += `chip revision : ${chip_revision}<br>`;
+		messageContainer.innerHTML += `counter = ${counter}<br>`;
+		break;
+	case 'frequencymeter':
+		if (json_data.t === 0x03 || json_data.t === 0x07) {
+			messageContainer.innerHTML += `Valeur champ ${json_data.t} : ${json_data.v}<br>`;
+	    		}
+		break;
+	case 'powermeter':
+	       if (json_data.t === 0x46 || json_data.t === 0xAB) {
+			messageContainer.innerHTML += `Valeur champ ${json_data.t} : ${json_data.v}<br>`;
+		}
+		break;
+        }
 });
