21554a78e81706df37a2f952ae6fe589062e4cdb
Fabrice DECROP LONGET <f4bjh_95@yahoo.fr>
2025-02-24 15:50:06 +0100

    Update meas_mgt.c

--- data/meas_mgt.c
+++ data/meas_mgt.c
@@ -1,95 +1,147 @@
-/*Demo implementations of State Machines
- *
- * This demo leverages a table driven approach and function pointers
- *
- * Example state machine to be implemented
- *
- *          +-----+      Event1        +-----+      Event2        +-----+
- *    O---->|  A  +------------------->|  B  +------------------->|  C  |
- *          +-----+                    +-----+                    +-----+
- *             ^                                                     |
- *             |                       Event3                        |
- *             +-----------------------------------------------------+
- *
- * States: A, B, C
- * Events: NoEvent (not shown, holding current state), Event1, Event2, Event3
- *
- * Partly leveraged the example here: http://web.archive.org/web/20160808120758/http://www.gedan.net/2009/03/18/finite-state-machine-matrix-style-c-implementation-function-pointers-addon/
- *
- * This sample code can be compiled and run using GCC.
- * >> gcc -o demo_state_machine demo_state_machine.c
- * >> ./demo_state_machine
- */
-
-#include <stdio.h>
-#include <assert.h>
+//#include <stdio.h>
+//#include <assert.h>
 
 // Definitions of state id's, event id's, and function pointer
-#define N_STATES  3
-#define N_EVENTS  4
+//#define N_STATES  3
+//#define N_EVENTS  4
+
 
 typedef enum {
-  STATE_A,
-  STATE_B,
-  STATE_C,
+  STATE_MEAS_INIT,
+  STATE_MEAS_PENDING,
+  STATE_MEAS_GET,
+  STATE_MEAS_CALC,
+  STATE_MEAS_FORMAT_JSON,
+  STATE_MEAS_PUSH_IN_QUEUE,
+  STATE_MEAS_REMOVE,
+  STATE_MEAS_ADD,
+  N_STATES,
 } StateId;
 
 typedef enum {
   NOEVENT,
-  EVENT1,
-  EVENT2,
-  EVENT3,
-} Event;
-typedef void (*StateRoutine)();
+  INIT_MEAS,
+  PULL_MEAS,    //event set by fsm task that will poll cpu1 data is measures are ready
+  PUSH_MEAS,    //event set by fsm that will push data in http server queue to upload measure to client
+  REMOVE_MEAS,  //event set by fsm that will remove measures (in case of error, or because it is pure static measures
+  N_EVENTS,
+} event_t;
 
-// Assert on number of states and events defined
-static_assert(STATE_C==N_STATES-1,
-  "Number of states does not match defined number of states");
-static_assert(EVENT3==N_EVENTS-1,
-  "Number of events does not match defined number of events");
+typedef struct {
+T_MEAS_NUMBER meas_num;
+event_t  ev;
+} q_msg_t;
+
+typedef struct{
+  boolean ready;   //measure is ready (set by cup1, reset by fsm)
+  size_t size;     //size of the measures (set by cpu1)
+  uint8_t *pdata;  //pointer to ram with measures result (set and ill by cpu1)
+} measures_t
+
+struct {
+ T_MEAS_NUMBER meas_num;
+ boolean once;
+ uint8_t retries;
+ measures_t measures;
+ char value[];
+ char *json_string;
+ esp_err_t (*init_func)(instance_data_t *);
+ esp_err_t (*calc_func)(instance_data_t *);
+} instance_data;
+
+typedef struct instance_data instance_data_t;
+typedef  esp_err_t (*state_func)(instance_data_t *);
 
 // Defining State, holds both state id and state routine
 typedef struct {
-    StateId id;
-    StateRoutine routine;
-}  State;
+    state_id id;
+    state_func func;
+}  state_t;
 
 // General functions
 void evaluate_state(Event e);
 
 // State routines to be executed at each state
-void state_routine_a(void);
-void state_routine_b(void);
-void state_routine_c(void);
+esp_err_t state_meas_init_func(instance_data_t *data);
+esp_err_t state_meas_get(instance_data_t *data);
+esp_err_t state_meas_calc(instance_data_t *data);
+esp_err_t state_meas_fromat_json(instance_data_t *data);
+esp_err_t state_meas_push_in_queue(instance_data_t *data);
+esp_err_t state_meas_pending(instance_data_t *data);
+esp_err_t state_meas_remove(instance_data_t *data);
+esp_err_t state_meas_add(instance_data_t *data);
 
 // Defining each state with associated state routine
-const State state_a = {STATE_A, state_routine_a};
-const State state_b = {STATE_B, state_routine_b};
-const State state_c = {STATE_C, state_routine_c};
+const state state_meas_init = {STATE_MEAS_INIT, state_meas_init_func};
+const state state_meas_get = {STATE_MEAS_GET, state_meas_get};
+const state state_meas_calc = {STATE_MEAS_CALC, state_meas_calc};
+const state state_meas_format_json = {STATE_FORMAT_JSON, state_meas_format_json};
+const state state_meas_push_in_queue = {STATE_PUSH_IN_QUEUE, state_meas_push_in_queue};
+const state state_meas_pending = {STATE_PENDING, state_meas_pending};
+const state state_meas_remove = {STATE_REMOVE, state_meas_remove};
+const state state_meas_add = {STATE_ADD, state_meas_add};
+
 
 // Defning state transition matrix as visualized in the header (events not
 // defined, result in mainting the same state)
-State state_transition_mat[N_STATES][N_EVENTS] = {
-   { state_a, state_b, state_a, state_a},
-   { state_b, state_b, state_c, state_b},
-   { state_c, state_c, state_c, state_a}};
+state_t state_transition_mat[N_STATES][N_EVENTS] = {
+   // NO_EVENT,                 INIT_MEAS,                PULL_MEAS,                PUSH_MEAS,                REMOVE_MEAS
+   { STATE_MEAS_PENDING,        STATE_MEAS_INIT,          STATE_MEAS_GET,           STATE_MEAS_PENDING,       STATE_MEAS_REMOVE},
+   { STATE_MEAS_INIT,           STATE_MEAS_INIT,          STATE_MEAS_GET,           STATE_MEAS_INIT,          STATE_MEAS_REMOVE},
+   { STATE_MEAS_GET,            STATE_MEAS_GET,           STATE_MEAS_GET,           STATE_MEAS_CALC,          STATE_MEAS_REMOVE},
+   { STATE_MEAS_CALC,           STATE_MEAS_CALC,          STATE_MEAS_CALC,          STATE_MEAS_FORMAT_JSON,   STATE_MEAS_REMOVE},
+   { STATE_MEAS_FORMAT_JSON,    STATE_MEAS_FORMAT_JSON,   STATE_MEAS_FORMAT_JSON,   STATE_MEAS_PUSH_IN_QUEUE, STATE_MEAS_REMOVE},
+   { STATE_MEAS_PUSH_IN_QUEUE,  STATE_MEAS_PUSH_IN_QUEUE, STATE_MEAS_GET,           STATE_MEAS_PUSH_IN_QUEUE, STATE_MEAS_REMOVE},
+   { STATE_MEAS_REMOVE,         STATE_MEAS_PENDING,       STATE_MEAS_REMOVE,        STATE_MEAS_REMOVE,        STATE_MEAS_REMOVE}};
+   
+
+T_DATA_TAG get_measurement_tag(instance_data_t *data)
+{
+T_DATA_TAG result;
+  switch (data->meas_num) {
+    case DATA_TO_SEND_IS_CHIP_NAME:
+      result = CHIP_INFO_MODEL_DATA_TAG;
+      break;
+
+  }
+
+  return result;
+}
+
 
 // Define current state and initialize
-State current_state = state_a;
+state_t current_state = STATE_MEAS_INIT;
 
 int main()
 {
+
+  instance_data_t data[DATA_TO_SEND_IS_LAST];
+  event_t ev;
+    q_msg_t q_msg;
+
+//initilaisation de data
+  for i=0; i<LAS_DATA_TO_SEND) {
+  data->meas_num = meas_num;
+  switch(meas_num) 
+    case DATA_TO_SEND_IS_CHIP_NAME: 
+       data->once=true;
+       init_func = 
+         }
+
     while(1) {
     // Event to receive from user
-    int ev;
+    
 
-    printf("----------------\n");
-    printf("Current state: %c\n", current_state.id + 65);
-    printf("Event to occur: ");
-    // Receive event from user
-    scanf("%u", &ev);
-    evaluate_state((Event) ev); // typecast to event enumeration type
-    printf("-----------------\n");
+
+    //read the queue
+    //get all the event recevied in queue
+    //2 bytes : [meas number T_DATA_TO_SEND] - [ event event_t]    perhaps, rename DATA_TO_SEND, in something likie measnumerment number
+
+    // par courir la queue
+   //  pour chaque evenement chopée dans la queue : 
+      xQueueReceive(queue, &q_msg , 0 ) == pdTRUE) {
+      evaluate_state(q_msg.ev, &data[q_msg.meas_num]);
+    
     };
     return (0);
 }
@@ -97,25 +149,119 @@ int main()
 /*
  * Determine state based on event and perform state routine
  */
-void evaluate_state(Event ev)
+void evaluate_state(event_t ev, instance_data_t **data)
 {
     //Determine state based on event
-  current_state = state_transition_mat[current_state.id][ev];
-  printf("Transitioned to state: %c\n", current_state.id + 65);
+    current_state = state_transition_mat[current_state.id][ev];
+    printf("Transitioned to state: %c\n", current_state.id + 65);
+  
     // Run state routine
-    (*current_state.routine)();
+    (*current_state.func)(*data); //valuer du pointeur vers data a revoir
 }
 
 /*
  * State routines
  */
-void state_routine_a() {
-  printf("State A routine ran. \n");
 
+esp_err_t (*state_meas_init)(instance_data_t * data)
+{ 
+
+data->init_func(data)
+
+
+  
+  return ESP_OK;
 }
-void state_routine_b() {
-  printf("State B routine ran. \n");
+
+esp_err_t (*state_meas_pending)(instance_data_t * data)
+{ 
+  q_msg_t q_msg;
+
+  if (data->once) {
+     q_msg.meas_num = data->meas_num;
+     q_msg.ev = REMOVE_MEAS;
+     xQueueSend(queue, &q_msg,0); //a affiner. time to wait ? nom de la queue fsm ? nom q_msg ?
+     }
+     
+  return ESP_OK;
 }
-void state_routine_c() {
-  printf("State C routine ran. \n");
+     
+
+
+esp_err_t (*state_meas_get)(instance_data_t * data)
+{ 
+
+
+if (data->measures.ready) {//it has been set by cpu 1
+  //copy ram measure zone of cpu1 into data->measures.pdata
+    q_msg.meas_num = data->meas_num;
+     q_msg.ev = PUSH_MEAS;
+     xQueueSend(queue, &q_msg,0); //a affiner. time to wait ? nom de la queue fsm ? nom q_msg ?
 }
+  
+  return ESP_OK;
+}
+
+esp_err_t (*state_meas_calc)(instance_data_t * data)
+{ 
+  data->calc_func(data);
+  return ESP_OK;
+}
+
+esp_err_t (*state_meas_format_json)(instance_data_t * data)
+{ 
+     cJSON *root;
+   json_data_t json_data; 
+  
+   sprintf(json_data.value,"%s", data->value);
+   json_data.length=strlen(data->json_data.value);
+	
+  
+		if (json_data.length !=0) {
+
+      json_data.tag = get_measurement_tag(data)
+      root = cJSON_CreateObject();
+		  set_json_data(root,&json_data);
+		  data->json_string = cJSON_Print(root);
+		  cJSON_Delete(root);
+      
+		}
+  
+  return ESP_OK;
+}
+
+
+esp_err_t (*state_meas_push_in_queue)(instance_data_t * data)
+{ 
+
+  
+
+   while( (xQueueSend( xQueue, data->json_string, xDelay ) == errQUEUE_FULL) && (retries <data->retries)) {
+	   retries++;
+	 }
+
+   //on retry on next current state wich will be the same
+   if (retries == data-> max_retries) {
+     q_msg.meas_num = data->meas_num;
+     q_msg.ev = PUSH_MEAS;
+     xQueueSend(queue, &q_msg,0); //a affiner. time to wait ? nom de la queue fsm ? nom q_msg ?
+   }
+   else if (data->once)
+      {
+     q_msg.meas_num = data->meas_num;
+     q_msg.ev = REMOVE_MEAS;
+     xQueueSend(queue, &q_msg,0); //a affiner. time to wait ? nom de la queue fsm ? nom q_msg ?
+   }
+   else {
+        q_msg.meas_num = data->meas_num;
+     q_msg.ev = PULL_MEAS;
+     xQueueSend(queue, &q_msg,0); //a affiner. time to wait ? nom de la queue fsm ? nom q_msg ?
+  
+   }
+     
+
+  
+
+  return ESP_OK;
+}
+
