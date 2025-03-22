1447cf5c5257d1e4043c9330ac6bc3158eae61f2
f4bjh <f4bjh_95@yahoo.fr>
2025-03-22 14:04:20 +0100

    34 - insert rf_gen measurement module

--- include/gpio.h
+++ include/gpio.h
@@ -1,4 +1,9 @@
 #define	GPIO_FREQUENCYMETER_INPUT	4
 
-#define GPIO_LCD_SDA           8
-#define GPIO_LCD_SCL           9
+#define GPIO_RF_GEN_CLK         5
+#define GPIO_RF_GEN_DATA	6
+#define GPIO_RF_GEN_CE		7
+#define GPIO_LCD_SDA            8
+#define GPIO_LCD_SCL            9
+#define GPIO_RF_GEN_LE		15
+#define GPIO_RF_GEN_PLL_LOCKED  16
--- include/meas.h
+++ include/meas.h
@@ -1,3 +1,26 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
+#include "freertos/event_groups.h"
+#include "esp_timer.h"
+#include "esp_log.h"
+#include "esp_system.h"
+#include <esp_http_server.h>
+#include <sys/param.h>
+#include "driver/spi_master.h"
+#include "driver/gpio.h"
+#include "driver/gptimer.h"
+#include "driver/pulse_cnt.h"
+
+#include "meas_mgt.h"
+#include "gpio.h"
+#include "adf4351.h"
+
 esp_err_t init_chip_info_model(meas_t *measure);
 esp_err_t  calc_chip_info_model(instance_meas_t *instance_meas);
 esp_err_t init_chip_revision(meas_t *measure);
@@ -33,3 +56,11 @@ esp_err_t init_frequencymeter(meas_t *measure);
 esp_err_t calc_frequencymeter(instance_meas_t *instance_meas);
 esp_err_t stop_frequencymeter(meas_t *measure);
 
+typedef struct rf_gen_task_arg_s {
+  meas_t *measure;
+  ADF4351_cfg_t vfo;
+}rf_gen_task_arg_t;
+esp_err_t init_rf_gen(meas_t *measure);
+esp_err_t stop_counter(meas_t *measure);
+esp_err_t calc_rf_gen(instance_meas_t *instance_meas);
+esp_err_t stop_rf_gen(meas_t *measure);
