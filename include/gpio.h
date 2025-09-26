4c75bcc64927956e97c5e38ec40048ca57108060
Fabrice DECROP LONGET <f4bjh_95@yahoo.fr>
2025-09-26 14:58:00 +0000

    130 - factorization of spi_bus_initialize to prepare use of same SPI bus to the FPGA of frequencymeter

--- include/gpio.h
+++ include/gpio.h
@@ -1,9 +1,17 @@
-#define	GPIO_FREQUENCYMETER_INPUT	4
+//bus SPI
+#define GPIO_SPI_MOSI   13
+//#define GPIO_SPI_MISO tbd
+#define GPIO_SPI_CLK    12 
 
-#define GPIO_RF_GEN_CLK         12
-#define GPIO_RF_GEN_DATA	13
-#define GPIO_RF_GEN_CE		7
-#define GPIO_LCD_SDA            8
-#define GPIO_LCD_SCL            9
+//frquencymeter
+#define	GPIO_FREQUENCYMETER_INPUT	4
+#define GPIO_FREQUENCYMETER_CE  tbd
+
+//rf generator
 #define GPIO_RF_GEN_LE		15
 #define GPIO_RF_GEN_PLL_LOCKED  16
+
+//LCD
+#define GPIO_LCD_SDA            8
+#define GPIO_LCD_SCL            9
+
