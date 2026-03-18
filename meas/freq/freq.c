07cb9ca60548c74ffb06bc700c571e8d16b39459
f4bjh <f4bjh_95@yahoo.fr>
2026-03-18 07:48:41 +0100

    131 - [meas/freq] FPGA version reformated into a major.minor

--- meas/freq/freq.c
+++ meas/freq/freq.c
@@ -165,17 +165,13 @@ esp_err_t init_frequencymeter(meas_t *measure)
 	ESP_LOGE(TAG,"reciproc_freq_read_status() failed!");
 	return ESP_FAIL;
     }
-    if (reciproc_freq_status[RECIPROC_FREQ_MEAS_BYTE_POS_VERSION] != RECIPROC_FREQ_MEAS_VERSION) {
+    if ((reciproc_freq_status[RECIPROC_FREQ_MEAS_BYTE_POS_MAJ_VERSION] != RECIPROC_FREQ_MEAS_MAJ_VERSION)  || 
+	(reciproc_freq_status[RECIPROC_FREQ_MEAS_BYTE_POS_MIN_VERSION] != RECIPROC_FREQ_MEAS_MIN_VERSION)) {
 	ESP_LOGE(TAG,"init_frequencymeter failed : reciproc_freq_meas fpga version incompatible (%d/%d)",reciproc_freq_status[RECIPROC_FREQ_MEAS_BYTE_POS_VERSION],RECIPROC_FREQ_MEAS_VERSION);
     	return ESP_FAIL;
     }
     if (reciproc_freq_status[RECIPROC_FREQ_MEAS_BYTE_POS_STATUS] & RECIPROC_FREQ_MEAS_BIT_POS_STATUS_ERROR_FLAG) {
 	ESP_LOGE(TAG,"init_frequencymeter failed : reciproc_freq_meas fpga in error state");
-
-    ESP_LOGI(TAG,"status=0x%02X %02X %s",reciproc_freq_status[3],RECIPROC_FREQ_MEAS_BIT_POS_STATUS_ERROR_FLAG,reciproc_freq_status[RECIPROC_FREQ_MEAS_BYTE_POS_STATUS] && RECIPROC_FREQ_MEAS_BIT_POS_STATUS_ERROR_FLAG ? "oui":"non" );
- 
-
-
 	return ESP_FAIL;
     }
  
